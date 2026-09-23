5bc3921995873de657c25b692822a4eaeb9c299f
jdarizasa <79489056+jdarizasa@users.noreply.github.com>
2026-09-23 13:45:45 +0000

    Adding libraries and functions

---
+++ calc/src/lib.rs
@@ -0,0 +1,20 @@
+// build calculator functions 
+pub fn add(a: f64, b: f64) -> f64 {
+    a + b
+}
+
+pub fn subtract(a: f64, b: f64) -> f64 {
+    a - b
+}
+
+pub fn multiply(a: f64, b: f64) -> f64 {
+    a * b
+}
+
+pub fn divide(a: f64, b: f64) -> Result<f64, &'static str> {
+    if b == 0.0 {
+        Err("Denominator cannot be zero")
+    } else {
+        Ok(a / b)
+    }
+}
---
+++ calc/src/main.rs
@@ -0,0 +1,44 @@
+// Web Microservice for calculating multiple operations
+use actix_web::{get, web, App, HttpServer, HttpResponse};
+
+#[get("/add/{a}/{b}")]
+async fn add(a: web::Path<(f64, f64)>) -> HttpResponse {
+    let (x, y) = a.into_inner();
+    HttpResponse::Ok().json(calc::add(x, y))
+}
+
+#[get("/subtract/{a}/{b}")]
+async fn subtract(a: web::Path<(f64, f64)>) -> HttpResponse {
+    let (x, y) = a.into_inner();
+    HttpResponse::Ok().json(calc::subtract(x, y))
+}
+
+#[get("/multiply/{a}/{b}")]
+async fn multiply(a: web::Path<(f64, f64)>) -> HttpResponse {
+    let (x, y) = a.into_inner();
+    HttpResponse::Ok().json(calc::multiply(x, y))
+}
+
+#[get("/divide/{a}/{b}")]
+async fn divide(a: web::Path<(f64, f64)>) -> HttpResponse {
+    let (x, y) = a.into_inner();
+    match calc::divide(x, y) {
+        Ok(result) => HttpResponse::Ok().json(result),
+        Err(error) => HttpResponse::BadRequest().json(error),
+    }
+}
+
+#[actix_web::main]
+
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(add)
+            .service(subtract)
+            .service(multiply)
+            .service(divide)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
